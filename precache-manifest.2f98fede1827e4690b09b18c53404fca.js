b642af3d1d68e96414287785450c398c831dd391
Michael Hallett <m.hallett@gmail.com>
2020-09-06 19:54:51 -0400

    Updates

---
+++ precache-manifest.2f98fede1827e4690b09b18c53404fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d8554d0807f51cc0a8821750ec6d9a",
+    "url": "/the-shoppies/index.html"
+  },
+  {
+    "revision": "b309e865e78d46fa3296",
+    "url": "/the-shoppies/static/css/main.c0ccc0c8.chunk.css"
+  },
+  {
+    "revision": "4b1a6f076419ee180ef0",
+    "url": "/the-shoppies/static/js/2.34357a07.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/the-shoppies/static/js/2.34357a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b309e865e78d46fa3296",
+    "url": "/the-shoppies/static/js/main.d5c6e0fe.chunk.js"
+  },
+  {
+    "revision": "3028237cd38fe8ab6725",
+    "url": "/the-shoppies/static/js/runtime-main.7a5fa803.js"
+  }
+]);
