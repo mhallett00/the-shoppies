a9c1b65aa4e42a32757473b1802ea3d2a16003db
Michael Hallett <63163884+mhallett00@users.noreply.github.com>
2020-09-07 19:15:38 -0400

    Changed conditional displays

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -6,22 +6,29 @@ import Loader from "./Loader";
 function SearchResults(props) {
   const { searchResults, searchParams, nominations, nominate, loading } = props;
 
-  const movies = searchResults
-    ? searchResults.map((movie, index) => {
-        return (
-          <SearchResultItem
-            key={index}
-            title={movie.Title}
-            year={movie.Year}
-            nominations={nominations}
-            nominate={nominate}
-          />
-        );
-      })
-    : "No Results!";
+  const movies = searchResults ? (
+    searchResults.map((movie, index) => {
+      return (
+        <SearchResultItem
+          key={index}
+          title={movie.Title}
+          year={movie.Year}
+          nominations={nominations}
+          nominate={nominate}
+        />
+      );
+    })
+  ) : (
+    <div className="bad-search">No Results!</div>
+  );
   return (
     <>
-      <h5>Search Results {searchParams && `for ${searchParams}`}</h5>
+      <h5>Search Results {searchParams && `for: ${searchParams}`}</h5>
+      {!searchParams && !loading && (
+        <div className="empty-searchfield">
+          Enter search in the field above!
+        </div>
+      )}
       {(loading && <Loader />) || <ul>{movies}</ul>}
     </>
   );
