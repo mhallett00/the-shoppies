ddaabd32c7c83062cf06a1cf04df7056ae05925a
Michael Hallett <63163884+mhallett00@users.noreply.github.com>
2020-09-07 19:10:48 -0400

    Changed banner display logic

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Alert from "react-bootstrap/Alert";
 
-function Banner() {
+function Banner(props) {
+  const { nominations } = props;
+
   return (
-    <Alert className="nomination-banner" variant="primary">
+    <Alert
+      show={nominations.length >= 5}
+      className="nomination-banner"
+      variant="primary"
+    >
       Congrats! You've nominated at least 5 movies!
     </Alert>
   );
